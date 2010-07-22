921397f5d104c71d6145b3b3f49f9c75cb7bea80
Rajit <rajit.b.singh@gmail.com>
2010-07-22 16:45:34 -0400

    Remove ->set_cache_record to hopefully avoid conflict

--- lib/Autocache/Strategy/Simple.pm
+++ lib/Autocache/Strategy/Simple.pm
@@ -25,7 +25,6 @@ sub get_cache_record
     {
         $self->_miss;
         $rec = $self->_create_cache_record( $name, $normaliser, $coderef, $args, $return_type );
-		$self->set_cache_record($rec);
     }
     else
     {
